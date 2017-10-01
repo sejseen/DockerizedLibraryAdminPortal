47c0547728a0c4971819c955bd4832d7ae8fd7ae
Konrad Tyma <Konrad.tyma@avra.com.pl>
2017-10-01 11:56:23 +0200

    Fixes for Ctrl

--- src/main/java/pwr/ktyma/libraryadminportal/adminservice/controller/BookController.java
+++ src/main/java/pwr/ktyma/libraryadminportal/adminservice/controller/BookController.java
@@ -2,6 +2,7 @@ package pwr.ktyma.libraryadminportal.adminservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
