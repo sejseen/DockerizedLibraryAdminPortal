e6790579f0afb5c3174dde22e1b5593cce1d3fe5
Konrad Tyma <Konrad.tyma@avra.com.pl>
2017-10-21 10:53:29 +0200

    Locale

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('.delete-book').on('click', function () {
+
+        /*<![CDATA[*/
+        var path =/*[[@{/}]]*/'remove';
+        /*]]>*/
+        var id=$(this).attr('id');
+
+        bootbox.confirm({
+            message: "Czy na pewno chcesz usunąć tą pozycję z portalu?",
+            buttons: {
+                cancel: {
+                    label:'<i class="fa fa-times"></i> Powrót'
+                },
+                confirm: {
+                    label:'<i class="fa fa-times"></i> Potwierdź'
+                }
+            },
+            callback: function (confirmed) {
+                if(confirmed) {
+                    $.post(path, {'id':id}, function (res) {
+                        location.reload();
+                    });
+                }
+            }
+        });
+    });
+})
